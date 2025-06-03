7fa967f454a3d817e7b37267ab97d4bce7eefa4c
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2025-06-02 21:11:49 -0600

    Prototype Pattern
    The prototype allows me to clone objects of the different types easily due to them having a common interface. In my code example, I have a bomb interface which each type of bomb implements. So in main, I can create objects of type bomb and then set them to be types of bomb or to be clones of other existing bomb objects.

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -12,6 +12,25 @@
             box.DoAction(); // DoAction will do that states action
 
             box.DoAction(); // JumpStates previous DoAction will change currentState of box to AirState. Now DoAction is AirState's DoAction.
-        }
+
+
+
+            //* Prototype Pattern *//
+            Bomb bigBomb = new BigBomb(6, 30);
+			Bomb newBigBomb1 = bigBomb.Clone();
+			bigBomb.Explode();
+			newBigBomb1.Explode();
+
+            Bomb newBigBomb2 = new BigBomb(8, 500);
+            newBigBomb2.Explode();
+
+			Bomb ceeFour1 = new C4(2, 10);
+            Bomb ceeFour2 = ceeFour1.Clone();
+			Bomb ceeFour3 = ceeFour1.Clone();
+
+            ceeFour1.Explode();
+            ceeFour2.Explode();
+            ceeFour3.Explode();
+		}
     }
 }
---
+++ FinalProject/PrototypePattern.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace FinalProject
+{
+	public interface Bomb // Prototype Interface
+	{
+		Bomb Clone();
+		void Explode();
+	}
+
+	class BigBomb : Bomb // Concrete Prototype
+	{
+		private int _explosionTimer;
+		private int _bombSize;
+
+		public BigBomb(int bombSize, int explosionTimer)
+		{
+			_bombSize = bombSize;
+			_explosionTimer = explosionTimer;
+		}
+
+		public Bomb Clone()
+		{
+			return new BigBomb(_bombSize, _explosionTimer);
+		}
+
+		public void Explode()
+		{
+			Debug.WriteLine("I am big bomb, kaboom! Explosion Timer = " + _explosionTimer + ", Bomb Size = " + _bombSize);
+		}
+	}
+
+	class C4 : Bomb
+	{
+		private int _bombRadius;
+		private int _bombSize;
+
+		public C4(int bombSize, int bombRadius)
+		{
+			_bombSize = bombSize;
+			_bombRadius = bombRadius;
+		}
+
+		public Bomb Clone()
+		{
+			return new C4(_bombSize, _bombRadius);
+		}
+
+		public void Explode()
+		{
+			Debug.WriteLine("I am C4, skadoosh! Bomb Radius = " + _bombRadius + ", Bomb Size = " +_bombSize);
+		}
+	}
+}
